b307d014acbf0b4d4f6f019b41b49f46c4042434
2239559319 <2017141461249@stu.scu.edu.cn>
2018-09-13 11:49:56 +0800

    remove

--- 文件处理/1.py
+++
@@ -1,7 +0,0 @@
-import builtwith
-import urllib3
-url="http://202.115.33.181:81/"
-print(builtwith.parse(url))
-'''
-获取网站的构架
-'''
--- 文件处理/4.py
+++
@@ -1,8 +0,0 @@
-import gevent
-
-list=[]
-def run(num):
-    print("coroutine%d is running"%num)
-if __name__=="__main__":
-    for i in range(1,6):
-        gevent.joinall(run(i))
